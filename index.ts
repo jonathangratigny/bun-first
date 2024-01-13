d5041b9ed0d3f2a73be67a3170f178e15e323f74
Jonathan2307 <gratigny.j@me.com>
2024-01-13 12:39:15 +0100

    hello!

--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-const t = 'world'
-console.log(`hello ${t}`)
+import figlet from 'figlet';
+
+
+const body = figlet.textSync('Hello DevGT!', {
+    font: 'Small Slant',
+    horizontalLayout: 'alternate',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true,
+    colors: true
+
+})
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(request: Request) {
+        return new Response(body);
+    },
+})
+
+console.log(`Listening on http://localhost:${server.port}/`);
